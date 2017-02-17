8dfd02cd1c136ead3dcf8ebe6cf6f1544b2d5a0f
Brian Barto <bartobrian@gmail.com>
2017-02-17 13:24:11 -0500

    Change license header to GPL
    	modified:   src/nms.c
    	modified:   src/nmscharset.c
    	modified:   src/nmscharset.h
    	modified:   src/nmseffect.c
    	modified:   src/nmseffect.h
    	modified:   src/nmstermio.c
    	modified:   src/nmstermio.h
    	modified:   src/nmstermio_ncurses.c
    	modified:   src/sneakers.c

--- src/sneakers.c
+++ src/sneakers.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2017 Brian Barto
  * 
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the MIT License. See LICENSE for more details.
+ * under the terms of the GPL License. See LICENSE for more details.
  */
 
 #include <stdio.h>
